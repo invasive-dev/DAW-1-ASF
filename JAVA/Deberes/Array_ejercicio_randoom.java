b46b85d3780850679518acaaf7466f30fa8ad3e3
adrian-sf <134473428+invasive-dev@users.noreply.github.com>
2023-11-23 23:01:44 +0100

    Update Array_ejercicio_randoom.java

--- JAVA/Deberes/Array_ejercicio_randoom.java
+++ JAVA/Deberes/Array_ejercicio_randoom.java
@@ -17,23 +17,23 @@ public class Array_ejercicio_randoom {
 
         // Poner los datos del array usando Random
         double min=0,max=10,range=max-min+1,nota; // variables del random con la nota min y la maxima
-        int list; // list en int para que no de lossy conversion al calcular con un int el list++ y el array que esta en double
-        for(list=0;list<array.length;list++){
+        int index; // index en int para que no de lossy conversion al calcular con un int el index++ y el array que esta en double
+        for(index=0;index<array.length;index++){
             nota = (double)(Math.random()*range)+min;
-            if(nota>10.0){ // para que la nota no sea 10.54 por ejemplo le asignamos la nota a 10 si es mayor que 10.0
-                nota=10.0;
+            if(nota>10){ // para que la nota no sea 10.54 por ejemplo le asignamos la nota a 10 si es mayor que 10.0
+                nota=10;
             }
-            array[list]=nota;
+            array[index]=nota;
         }
 
-        // Loop acumulativo del list entero del array que tambien imprime el list como tal sin acumular
-        for(list=0; list<array.length;list++){ // for que empieza en 0 y acaba cuando ya no sea menor a la cantidad de valores en el array, se va sumando por iteracion
-            System.out.println(decimal.format(array[list])+"/10.00"); // imprime el valor numero list del numero del array por iteracion list++ = 0,1,2,3,4 por lo tanto nota[0],nota[1],nota[2],nota[3],etc...
-            suma+= array[list]; // imprime la acumulacion por iteracion: nota[1] 0+nota[0]=8.78, 8.78+nota[1]=4,59, 13.37+nota[2]=6,30 etc...
+        // Loop acumulativo del index entero del array que tambien imprime el index como tal sin acumular
+        for(index=0; index<array.length;index++){ // for que empieza en 0 y acaba cuando ya no sea menor a la cantidad de valores en el array, se va sumando por iteracion
+            System.out.println(decimal.format(array[index])+"/10"); // imprime el valor numero index del numero del array por iteracion index++ = 0,1,2,3,4 por lo tanto nota[0],nota[1],nota[2],nota[3],etc...
+            suma+= array[index]; // imprime la acumulacion por iteracion: nota[1] 0+nota[0]=8.78, 8.78+nota[1]=4,59, 13.37+nota[2]=6,30 etc...
         }
         // Calculo y mostrar por pantalla
         media = suma/array.length; // media que usa el total de todos los valores sumados dividido entre la cantidad de valores en el array
         System.out.println("");
-        System.out.println("Media: " + decimal.format(media) + "/10.00"); // mostramos con el formato por dos decimales la media por pantalla
+        System.out.println("Media: " + decimal.format(media) + "/10"); // mostramos con el formato por dos decimales la media por pantalla
     }
 }
